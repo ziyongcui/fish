657e4b1e88f4955c5b48472bf31e095d488ae7bf
Alek Westover <alek.westover@gmail.com>
2020-07-16 17:27:41 -0400

    app

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
- 
+from flask import Flask, requests, render_template, session
+
+app = Flask(__name__)
+
+@app.route("/")
+def serve_index():
+    return render_template("index.html")
+
+
+# TODO: not really this
+@app.route("/game")
+def serve_index():
+    return render_template("game.html")
+
 
